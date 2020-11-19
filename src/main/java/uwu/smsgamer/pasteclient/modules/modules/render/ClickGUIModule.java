17dad5649349e8c4efa6dda2399cd0cc0a62fc88
true-cc <creepercannyon@gmail.com>
2020-11-19 08:43:55 -0800

    Removed SesqClickGUI

--- src/main/java/uwu/smsgamer/pasteclient/modules/modules/render/ClickGUIModule.java
+++ src/main/java/uwu/smsgamer/pasteclient/modules/modules/render/ClickGUIModule.java
@@ -19,8 +19,7 @@ public class ClickGUIModule extends Module {
     public static final ClickGUI clickGui = new ClickGUI();
 
     private final IntChoiceValue value = addIntChoice("Mode", "Mode for clickgui", 0,
-      0, "Block",
-      1, "Sesq");
+      0, "Block");
 
     public ClickGUIModule() {
         super("ClickGUI", "The click gui", ModuleCategory.RENDER, true, false, Keyboard.KEY_RSHIFT);
