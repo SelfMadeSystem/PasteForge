d63e94b48f131999c7baaca599f940df833e6faf
true-cc <creepercannyon@gmail.com>
2021-01-10 18:03:23 -0800

    Starting to add shit to Fly.java

--- src/main/java/uwu/smsgamer/pasteclient/modules/modules/movement/Fly.java
+++ src/main/java/uwu/smsgamer/pasteclient/modules/modules/movement/Fly.java
@@ -2,11 +2,14 @@ package uwu.smsgamer.pasteclient.modules.modules.movement;
 
 import com.darkmagician6.eventapi.EventTarget;
 import com.darkmagician6.eventapi.types.EventType;
+import com.google.gson.JsonElement;
 import net.minecraft.network.play.client.CPacketPlayer;
 import uwu.smsgamer.pasteclient.events.*;
 import uwu.smsgamer.pasteclient.modules.*;
 import uwu.smsgamer.pasteclient.utils.*;
 import uwu.smsgamer.pasteclient.values.*;
+
+import java.util.Map;
 
 public class Fly extends PasteModule {
     private final IntChoiceValue mode = addIntChoice("Mode", "Mode for fly", 0,
@@ -17,6 +20,7 @@ public class Fly extends PasteModule {
     private final BoolValue ground = addBool("Ground", "Sets ground client side.", false);
     private final BoolValue groundSpoof = addBool("GroundSpoof", "Spoofs your ground.", false);
     private final BoolValue groundSpoofM = addBool("GroundSpoofM", "Sets spoof ground to this.", false);
+    private final AdvancedSettings advanced = (AdvancedSettings) addValue(new AdvancedSettings("Advanced settings", "idk todo: this")); // todo: this
 
     public Fly() {
         super("Fly", "Lets you fly.", ModuleCategory.MOVEMENT);
@@ -79,6 +83,100 @@ public class Fly extends PasteModule {
                 if (spoofGround) builder.setOnGround(spoofGroundM);
                 event.setPacket(builder.build());
             }
+        }
+    }
+
+    private static class AdvancedSettings extends ChildGen {
+
+        public AdvancedSettings(String name, String description) {
+            super(name, description);
+        }
+
+        @Override
+        public void genChildren(Value<?> parentValue) {
+            parentValue.addChild(new ActionSettings());
+        }
+
+        @Override
+        public Value<?> genMainChild() {
+            StringBuilder sb = new StringBuilder("Event");
+            for (int i = 0; i <= children.size(); i++) {
+                sb.append(i);
+                if (!hasChild(sb.toString())) break;
+                sb.setLength(sb.length() - String.valueOf(i).length());
+            }
+            VoidValue val = new VoidValue(sb.toString(), "") {
+                @Override
+                public boolean rightClickRemove() {
+                    return true;
+                }
+            };
+            val.setParent(this);
+            return val;
+        }
+
+        @Override
+        public void loadFromJSON(Map.Entry<String, JsonElement> entry) {
+            // TODO
+        }
+    }
+
+    private static class ActionSettings extends ChildGen {
+
+        public ActionSettings() {
+            super("Action Settings", "Customize the actions of this event.");
+        }
+
+        @Override
+        public void genChildren(Value<?> parentValue) {
+            BoolValue v;
+            parentValue.addChild(v = new BoolValue("Packet", "Send packet to position.", false));
+            parentValue.addChild(new BoolValue("ToFloor", "Teleports to floor.", false) {
+                @Override
+                public String getDescription() {
+                    return v.getValue() ? "Sends packet to floor." : "Teleports to floor.";
+                }
+            });
+            parentValue.addChild(new PositionValue("Position", "How to set the position.", false));
+            parentValue.addChild(new RangeValue("Timer",
+              "To set the timer to (random between two points). Set to 0 to have no effect.",
+              1, 1, 0, 10, 0.05, NumberValue.NumberType.DECIMAL));
+            parentValue.addChild(new IntChoiceValue("MotionXZType", "The type of motion to set for XZ.", 0,
+              new StringHashMap<>(
+                0, "Set",
+                1, "Add",
+                2, "Times"
+              )));
+            parentValue.addChild(new IntChoiceValue("MotionYType", "The type of motion to set for Y.", 0,
+              new StringHashMap<>(
+                0, "Set",
+                1, "Add",
+                2, "Times"
+              )));
+            parentValue.addChild(new PositionValue("Motion", "The motion to set.", false));
+        }
+
+        @Override
+        public Value<?> genMainChild() {
+            StringBuilder sb = new StringBuilder("Action");
+            for (int i = 0; i <= children.size(); i++) {
+                sb.append(i);
+                if (!hasChild(sb.toString())) break;
+                sb.setLength(sb.length() - String.valueOf(i).length());
+            }
+            VoidValue val = new VoidValue(sb.toString(), "") {
+                @Override
+                public boolean rightClickRemove() {
+                    return true;
+                }
+            };
+            val.setParent(this);
+            return val;
+        }
+
+        @Override
+        public void loadFromJSON(Map.Entry<String, JsonElement> entry) {
+            // TODO
         }
     }
 }
